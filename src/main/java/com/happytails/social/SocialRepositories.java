e5c099f4a0a9e27b50fe56cc2fca40c7fb2de462
BhavanaAngadi123 <bangadi@rivier.edu>
2026-08-19 11:46:38 -0400

    Add social network repositories

---
+++ src/main/java/com/happytails/social/SocialRepositories.java
@@ -0,0 +1,15 @@
+package com.happytails.social;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+interface PetProfileRepository extends JpaRepository<PetProfile,Long>{ Optional<PetProfile> findByHandle(String handle); }
+interface SocialPostRepository extends JpaRepository<SocialPost,Long>{ List<SocialPost> findByPetProfileIdOrderByCreatedAtDesc(Long petProfileId); }
+interface PetFollowRepository extends JpaRepository<PetFollow,Long>{ List<PetFollow> findByFollowerPetId(Long id); List<PetFollow> findByFollowingPetId(Long id); boolean existsByFollowerPetIdAndFollowingPetId(Long a,Long b); }
+interface FriendRequestRepository extends JpaRepository<FriendRequest,Long>{ List<FriendRequest> findByToPetIdAndStatus(Long id,String status); }
+interface PetReminderRepository extends JpaRepository<PetReminder,Long>{ List<PetReminder> findByPetProfileIdOrderByDueDateAsc(Long id); }
+interface PlayDateRepository extends JpaRepository<PlayDate,Long>{ List<PlayDate> findByHostPetIdOrGuestPetIdOrderByScheduledAtAsc(Long a,Long b); }
+interface MeetupRepository extends JpaRepository<Meetup,Long>{ List<Meetup> findAllByOrderByScheduledAtAsc(); }
+interface PetSitterRepository extends JpaRepository<PetSitter,Long>{ List<PetSitter> findByAvailableTrue(); }
+interface PetOrderRepository extends JpaRepository<PetOrder,Long>{ List<PetOrder> findByPetProfileIdOrderByCreatedAtDesc(Long id); }
+interface PetMemoryRepository extends JpaRepository<PetMemory,Long>{ List<PetMemory> findByPetProfileIdOrderByMemoryDateDesc(Long id); }
